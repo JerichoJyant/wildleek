23f4af9a92295bc266158953aee0393a261caad4
Josh Patton <devnamedjosh@gmail.com>
2020-05-05 16:07:22 -0400

    Attempt to prevent passwords being loaded more than once -- will test

--- index.js
+++ index.js
@@ -5,28 +5,34 @@ const readline = require('readline');
 
 let passwordList = [];
 let passwordListLoaded = false;
-let passwordLI
+
+let loadingPromise = null;
+
 function loadPasswords() {
-    const passwordsFilePath = path.join(__dirname, 'passwords', '10-million-password-list-top-10000_ALPHABETIZED.txt');
-    return new Promise((resolve, reject) => {
-        const rl = readline.createInterface({
-            input: fs.createReadStream(passwordsFilePath),
-            crlfDelay: Infinity
-        });
+    if (loadingPromise === null) {
+        const passwordsFilePath = path.join(__dirname, 'passwords', '10-million-password-list-top-10000_ALPHABETIZED.txt');
+        loadingPromise = new Promise(
+            (resolve, reject) => {
+                const rl = readline.createInterface({
+                    input: fs.createReadStream(passwordsFilePath),
+                    crlfDelay: Infinity
+                });
 
-        rl.on('line', (line) => {
-            passwordList.push(line);
-        });
+                rl.on('line', (line) => {
+                    passwordList.push(line);
+                });
 
-        rl.on('close', () => {
-            passwordListLoaded = true;
-            resolve();
-        });
-    });
+                rl.on('close', () => {
+                    passwordListLoaded = true;
+                    resolve();
+                });
+            });
+    }
+    return loadingPromise;
 }
 
 async function passwordInTheWild(password) {
-    if(!passwordListLoaded) {
+    if (!passwordListLoaded) {
         await loadPasswords();
     }
     const searchResult = binary_search(passwordList, password, (a, b) => a.localeCompare(b));
